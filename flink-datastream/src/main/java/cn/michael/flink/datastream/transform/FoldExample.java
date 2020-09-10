bcfcafa2d18d01ec85f8d5519a1f0ba6471ba246
胡峰刚 <hufenggang@accesscorporate.com>
2020-09-10 22:03:39 +0800

    feat: 代码样例

--- flink-datastream/src/main/java/cn/michael/flink/datastream/transform/FoldExample.java
+++ flink-datastream/src/main/java/cn/michael/flink/datastream/transform/FoldExample.java
@@ -1,16 +1,15 @@
 package cn.michael.flink.datastream.transform;
 
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.common.functions.FoldFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * Created by hufenggang on 2020/9/4.
+ * Created by hufenggang on 2020/9/6.
  */
-public class KeyByExample {
+public class FoldExample {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -22,8 +21,14 @@ public class KeyByExample {
             new Tuple2<>("c", 2));
 
         KeyedStream<Tuple2<String, Integer>, String> keyedStream = dataStream.keyBy(value -> value.f0);
-        keyedStream.reduce((ReduceFunction<Tuple2<String, Integer>>)(value1, value2) ->
-            new Tuple2<>(value1.f0, value1.f1 + value2.f1)).print();
-        env.execute();
+        keyedStream.fold("start", new FoldFunction<Tuple2<String, Integer>, String>() {
+            @Override
+            public String fold(String s, Tuple2<String, Integer> o) throws Exception {
+                return s + "-" + o;
+            }
+        }).print();
+
+        env.execute("FoldExample");
+
     }
 }
--- flink-datastream/src/main/java/cn/michael/flink/datastream/transform/ReduceExample.java
+++ flink-datastream/src/main/java/cn/michael/flink/datastream/transform/ReduceExample.java
@@ -1,16 +1,15 @@
 package cn.michael.flink.datastream.transform;
 
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * Created by hufenggang on 2020/9/4.
+ * Created by hufenggang on 2020/9/6.
  */
-public class KeyByExample {
+public class ReduceExample {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -22,8 +21,14 @@ public class KeyByExample {
             new Tuple2<>("c", 2));
 
         KeyedStream<Tuple2<String, Integer>, String> keyedStream = dataStream.keyBy(value -> value.f0);
-        keyedStream.reduce((ReduceFunction<Tuple2<String, Integer>>)(value1, value2) ->
-            new Tuple2<>(value1.f0, value1.f1 + value2.f1)).print();
-        env.execute();
+
+        keyedStream.reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t1, Tuple2<String, Integer> t2) throws Exception {
+                return new Tuple2<>(t1.f0, t1.f1 + t2.f1);
+            }
+        }).print();
+
+        env.execute("ReduceExample");
     }
 }
